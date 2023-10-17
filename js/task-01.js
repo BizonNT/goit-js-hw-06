3fdc509e0222b195d0f67e84b16dfc24e73ee2bb
BizonNT <137486029+BizonNT@users.noreply.github.com>
2023-10-18 01:05:09 +0300

    Task-02, -03

--- js/task-01.js
+++ js/task-01.js
@@ -3,10 +3,7 @@ const categoriesCount = document.querySelectorAll(".item");
 console.log(`Number of categories: ${categoriesCount.length}`);
 
 categoriesCount.forEach(function (category, index) {
-  const categoryName = category.querySelector("h2").textContent
+  const categoryName = category.querySelector("h2").textContent;
   const elementsCount = category.lastElementChild.childElementCount;
   console.log(`Category: ${categoryName} \nElements: ${elementsCount}`);
 });
-
-
-
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+createMarkup(ingredients);
+
+function createMarkup(args) {
+  const li = args.map((arg) => {
+    const liEl = document.createElement("li");
+    liEl.textContent = arg;
+    liEl.classList.add("item");
+    return liEl;
+  });
+  list.append(...li);
+}
