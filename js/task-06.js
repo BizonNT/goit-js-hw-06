8309dfb7c7738dc998536a85e212250aafd57a12
BizonNT <137486029+BizonNT@users.noreply.github.com>
2023-10-20 05:28:00 +0300

    Changed task 06

--- js/task-06.js
+++ js/task-06.js
@@ -4,10 +4,10 @@ inputForm.addEventListener("blur", handleBlur);
 
 function handleBlur(event) {
   let validationInput = event.currentTarget;
+  validationInput.classList.add("invalid");
   if (validationInput.value.length === Number(validationInput.dataset.length)) {
     validationInput.classList.replace("invalid", "valid");
   } else {
     validationInput.classList.remove("valid");
-    validationInput.classList.add("invalid");
   }
 }
